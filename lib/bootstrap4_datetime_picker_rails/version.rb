882df88c3057b0f4a1f8dc9f2eb240b30ad8d88e
Kyle Fagan <kfagan@mitre.org>
2018-04-03 10:27:26 -0400

    Update to alpha 18 release

--- lib/bootstrap4_datetime_picker_rails/version.rb
+++ lib/bootstrap4_datetime_picker_rails/version.rb
@@ -1,6 +1,6 @@
 module Bootstrap4DatetimePickerRails
   module Rails
-    VERSION = '0.1.5'
-    TEMPUS_DOMINUS_VERSION = '5.0.0-alpha.17'
+    VERSION = '0.1.6'
+    TEMPUS_DOMINUS_VERSION = '5.0.0-alpha18'
   end
 end
